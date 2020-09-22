61b0a629554e5f63299ea45134fa26f81ccc7829
SJLEE316 <lsj8249@naver.com>
2020-09-23 01:26:38 +0900

    single page scroller and buttons

---
+++ demo/src/FirstComponent.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div className="component first-component">
+      <h2>First Componentdfdf</h2>
+      <h2>First Componentdfdf</h2>
+      <h2>First Componentdfdf</h2>
+      <h2>First Componentdfdf</h2>
+      <h2>First Componentdfdf</h2>
+      <h2>First Componentdfdf</h2>
+      <h2>First Componentdfdf</h2>
+      <h2>First Componentdfdf</h2>
+      <h2>First Componentdfdf</h2>
+      <h2>First Componentdfdf</h2>
+      <h2>First Componentdfdf</h2>
+      <h2>First Componentdfdf</h2>
+      <h2>First Componentdfdf</h2>
+      <h2>First Componentdfdf</h2>
+      <h2>First Componentdfdf</h2>
+      <h2>First Componentdfdf</h2>
+
+    </div>
+  );
+};
