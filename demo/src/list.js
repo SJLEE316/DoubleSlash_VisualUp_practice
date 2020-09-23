1ced90e7dbe356af3c4b1bce08638238709013bf
lim-jeongmin <62532871+lim-jeongmin@users.noreply.github.com>
2020-09-23 22:16:39 +0900

    Update list.js

--- demo/src/list.js
+++ demo/src/list.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class list extends Component {
     render() {
         return (
-            <div>
+            <h1>
                 List
-            </div>
+            </h1>
         );
     }
 }
