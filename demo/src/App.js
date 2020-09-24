616b819ee55107d90483729dad310bcaa3b6c1d8
lim-jeongmin <62532871+lim-jeongmin@users.noreply.github.com>
2020-09-24 18:00:58 +0900

    Update App.js

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,22 +1,63 @@
-import React from "react";
-import { HashRouter as Router, Route, Switch, Link, HashRouter } from "react-router-dom";
-import FullPage from "./FullPage";
-import login from "./login";
-import list from "./list";
-import search from "./search";
-import Navigation from "./Navigation";
+import React, {Component} from "react";
+import {Route, Link} from 'react-router-dom';
 
-function App() {
-  return ( 
-      <HashRouter>
-        <Navigation />
-        <Route exact path="/" component={FullPage} />
-        <Route path="/login" component={login} />
-        <Route path="/link" component={list} />
-        <Route path="/search" component={search} />
-      </HashRouter>
-     
-  );
-}
+import List from "./templates/List";
+import Login from "./templates/Login";
+import Search from "./templates/Search";
+import FullPage from "./templates/FullPage"
+import Navigation from "./templates/Navigation";
+
+
+const App = () => {
+    return(
+        <div>
+   {   /*      <ul>
+                <li><Link to="/">Visual up</Link></li>
+                <li><Link to="/list">리스트</Link></li>
+                <li><Link to="/login">로그인</Link></li>
+                <li><Link to="/search">검색</Link></li>
+            </ul>
+*/}         <Navigation/>
+            <Route path="/" component={FullPage}  exact={true}/>
+            <Route path="/list" component={List}/>
+            <Route path="/search" component={Search} />
+            <Route path="/login" component={Login}/>
+        </div>
+    );
+};
 
 export default App;
+
+
+
+{/*import React, {Component} from 'react';
+import { HashRouter as Router, Route, Switch, Link, HashRouter } from "react-router-dom";
+import FullPage from "./router/FullPage";
+import Login from "./router/Login";
+import List from "./router/List";
+import Search from "./router/Search";
+import Navigation from "./router/Navigation";
+
+
+class App extends Component {
+    render(){
+     return (
+        <Router basename="/demos">
+          <div>
+          <Navigation />
+            <Switch>
+                <FullPage />
+                <Route  path="/" exact={true} component={FullPage} />
+                <Route path="/login" component={Login} />
+                <Route path="/list" component={List} />
+                <Route path="/search" component={Search} />
+            </Switch>
+          </div>
+        </Router>
+       );
+     }
+   }
+   
+export default App;
+
+  */}
