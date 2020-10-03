86411ecad1be90607ca9bb2cab7685de1f65b3f6
SJLEE316 <lsj8249@naver.com>
2020-10-03 23:29:37 +0900

    명예의전당 step1

--- demo/src/FifthComponent.js
+++ demo/src/FifthComponent.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, {Component} from "react";
 import "./FifthComponent.css"
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-function FifthComponent() {
-  return (
-    <div className="component fifth-component">
-      <h2>Fifth Component</h2>
-    </div>
-  );
-};
-
-export default FifthComponent;
+export default class FifthComponent extends Component {
+  render() {
+    const settings = {
+      className: "center",
+      centerMode: true,
+      infinite: true,
+      centerPadding: "60px",
+      slidesToShow: 3,
+      speed: 500
+    };
+    return (
+      <div class="components fifth-component">
+        <h1>Center Mode</h1>
+        <div class="slider">
+          <Slider {...settings}>
+            <div class="card-container">
+              <h3>1</h3>
+            </div>
+            <div class="card-container">
+              <h3>2</h3>
+            </div>
+            <div class="card-container">
+              <h3>3</h3>
+            </div>
+            <div class="card-container">
+              <h3>4</h3>
+            </div>
+            <div class="card-container">
+              <h3>5</h3>
+            </div>
+            <div class="card-container">
+              <h3>6</h3>
+            </div>
+          </Slider>
+        </div>
+      </div>
+    );
+  }
+}
